732a6826ea972adf3ce4fa8a254ed48502f7368d
Sebastian Nilsson <sebastian.nilsson@nordiskagalleriet.com>
2022-11-11 13:25:53 +0100

    Streamline dev reinstall script

--- Dev-Reinstall.ps1
+++ Dev-Reinstall.ps1
@@ -1,9 +1,11 @@
-$csproj = 'src/DotnetGuid/DotnetGuid.csproj'
+$package = 'dotnet-guid'
+$project = 'DotnetGuid'
+$csproj = "src/${project}/${project}.csproj"
 
-dotnet tool uninstall -g dotnet-guid
+dotnet tool uninstall -g $package
 
 dotnet pack $csproj --output ./nupkg
 
 $version = ([Xml] (Get-Content $csproj)).Project.PropertyGroup.Version
 
-dotnet tool install -g dotnet-guid --add-source 'nupkg/' --version $version
+dotnet tool install -g $package --add-source 'nupkg/' --version $version
